3d0cf4562fdd02452b76de0d39c56bb455c4c9e8
Joshua Cheung <JTCheung97@gmail.com>
2026-04-25 17:42:32 -0700

    feat: add sticky Navbar with active link highlighting

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { Link, NavLink } from 'react-router-dom';
+import styles from './Navbar.module.css';
+
+export default function Navbar() {
+  return (
+    <header className={styles.header}>
+      <Link to="/" className={styles.logo}>
+        Dailo Doodles
+      </Link>
+      <nav className={styles.nav}>
+        <NavLink to="/" end className={({ isActive }) => isActive ? styles.activeLink : styles.link}>
+          Shop
+        </NavLink>
+        <NavLink to="/about" className={({ isActive }) => isActive ? styles.activeLink : styles.link}>
+          About
+        </NavLink>
+        <NavLink to="/faq" className={({ isActive }) => isActive ? styles.activeLink : styles.link}>
+          FAQ
+        </NavLink>
+        <NavLink to="/shipping" className={({ isActive }) => isActive ? styles.activeLink : styles.link}>
+          Shipping
+        </NavLink>
+        <NavLink to="/custom-order" className={({ isActive }) => isActive ? styles.activeLink : styles.link}>
+          Custom Order
+        </NavLink>
+      </nav>
+    </header>
+  );
+}
